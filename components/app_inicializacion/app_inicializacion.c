3e10d41bfc15816bd518bfcdcfa041dc679344e1
yeerliin <yeerliin@hotmail.es>
2025-05-24 18:00:09 +0200

    Corregido  inicializacion de app + wifi
    He corregido un problema que tenia con el wifi y el provision es algo chapucero que hay que mejorar mas adelante, pero para comenzar es pasable.
    
    Con esto lo que hemos conseguido es que se inicie el wifi guardado en la nvs despues del primer inicio y de siempre que se desconecte de la luz.

--- components/app_inicializacion/app_inicializacion.c
+++ components/app_inicializacion/app_inicializacion.c
@@ -44,17 +44,6 @@ esp_err_t inicializar_componentes(void)
 
     // Pausa corta
     vTaskDelay(pdMS_TO_TICKS(100));
-    
-    /*/ 5. Inicializar BLE Scanner
-    ESP_LOGI(TAG, "Inicializando escáner BLE...");
-    ret = ble_scanner_iniciar(NULL);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Error crítico al inicializar el escáner BLE: %s", esp_err_to_name(ret));
-        return ret;
-    }*/
-    // Pausa corta
-    vTaskDelay(pdMS_TO_TICKS(100));
-
          
     // 6. Inicializar el control boton (solo inicialización interna)
     ESP_LOGI(TAG, "Inicializando control por boton...");
@@ -66,14 +55,6 @@ esp_err_t inicializar_componentes(void)
 
     // Pausa corta
     vTaskDelay(pdMS_TO_TICKS(100));
-
-    // 7. Inicializar el controlador de aplicación (solo inicialización interna)
-    ESP_LOGI(TAG, "Inicializando controlador de aplicación...");
-    ret = app_control_iniciar_estado();
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Error al inicializar el controlador de aplicación: %s", esp_err_to_name(ret));
-        return ret;
-    }
 
     ESP_LOGI(TAG, "Todos los componentes inicializados correctamente");
     return ESP_OK;
